138a948af8939f822fed9a120fa893e5ee504557
Mustafa "Moose" Paksoy <paksoy@gmail.com>
2023-10-30 12:38:43 -0700

    wip

--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
-// Write a function that returns the number of true in an array
+// Write a function that returns the number of items that are
+// equal to 'true' in 'array'
 //
 // ex. countTrue([true, false]) -> 1
 function countTrue(array) {
@@ -13,4 +14,21 @@ test("countTrue()", () => {
   expect(countTrue([true, false, false, true, true, true, true])).toEqual(5);
   expect(countTrue([1])).toEqual(0);
   expect(countTrue(["true"])).toEqual(0);
+});
+
+// Write a function that return the number of times 'item' appears
+// in 'array'
+//
+// ex. countItem('apple', ['orange', 'apple', 'apple', 'apple']) -> 3
+// ex. countItem('orange', ['orange', 'apple', 'apple', 'apple']) -> 1
+function countItem(item, array) {
+  // YOUR CODE HERE
+}
+
+test("countItem()", () => {
+  expect(countItem(1, [])).toEqual(0);
+  expect(countItem(false, [])).toEqual(0);
+  expect(countItem(12, [1, 2, 3, 4, 5, 6, 12])).toEqual(1);
+  expect(countItem(12, [1, 2, 3, 4, 12, 5, 6, 12, 12, 12])).toEqual(4);
+  expect(countItem(false, [1, 2, 3, false, true, false, 1, 0])).toEqual(2);
 });
